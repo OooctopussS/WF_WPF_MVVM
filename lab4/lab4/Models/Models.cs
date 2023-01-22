dcb4e1f6297833ceaa34f10eb0fe94c485301ae0
Octopus <35968157+OooctopussS@users.noreply.github.com>
2023-01-22 22:48:36 +0300

    Add files via upload

---
+++ lab4/lab4/Models/Models.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+
+namespace lab4.Models
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+
+        public int Count { get; set; }
+
+        public int TimeToBuy { get; set; }
+
+        public Product(Product newProduct)
+        {
+            Name = newProduct.Name;
+            Count = newProduct.Count;
+            TimeToBuy = newProduct.TimeToBuy;
+        }
+        public Product()
+        {
+
+        }
+    }
+
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; } // 60>= пенсионер
+        public DateTime QueueEnterTime { get; set; }
+        public int BuyTime { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public int Prioritet => Age >= 60 && Gender == "Женщина" ? 3 : Age >= 60 && Gender == "Мужчина" ? 2 : Gender == "Женщина" ? 1 : 0;
+
+        public Customer(string name, string gender, int age, DateTime queueEnterTime)
+        {
+            Name = name;
+            Gender = gender;
+            Age = age;
+            QueueEnterTime = queueEnterTime;
+        }
+
+        public Customer()
+        {
+
+        }
+    }
+
+    internal class PriorityQueue
+    {
+       public ICollection<Customer> Queue { get; set; }
+
+        public void Add_SortQueue(Customer newCustomer)
+        {
+            LinkedList<Customer> SortedQueue;
+            if (Queue.Count > 0)
+            {
+                SortedQueue = new LinkedList<Customer>(Queue);
+                var item = SortedQueue.Last;
+                while (newCustomer.Prioritet > item.Value.Prioritet)
+                {
+                    if (item.Previous != null)
+                    {
+                        item = item.Previous;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                SortedQueue.AddAfter(item, newCustomer);
+            }
+            else
+            {
+                SortedQueue = new LinkedList<Customer>();
+                SortedQueue.AddLast(newCustomer);
+            }
+            Queue.Clear();
+           
+            foreach(Customer el in SortedQueue)
+            {
+                Queue.Add(el);
+            }
+        }
+
+        public PriorityQueue(ObservableCollection<Customer> customers)
+        {
+            Queue = new ObservableCollection<Customer>(customers);
+        }
+
+        public PriorityQueue()
+        {
+            Queue = new ObservableCollection<Customer>();
+        }
+
+    }
+    
+}
---
+++ lab4/lab4/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab4.ViewModels.Base;
+using System.Collections.ObjectModel;
+using lab4.Models;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Input;
+using lab4.Commands;
+using System.Windows.Data;
+
+namespace lab4.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        public DispatcherTimer AddCustomerTimer { get; }
+        public DispatcherTimer QueueServiceTimer { get; }
+        public PriorityQueue MainQueue { get; }
+        public List<Product> AddMenuProducts { get; }
+        public ObservableCollection<Customer> ServedCustomersList { get; }
+        public ObservableCollection<Product> ProductsSalesStatisic { get; }
+
+
+        #region Панель добавления - видимость
+        private Visibility _VisibleAddPanel = Visibility.Collapsed;
+
+        public Visibility VisibleAddPanel
+        {
+            get => _VisibleAddPanel;
+            set => Set(ref _VisibleAddPanel, value);
+        }
+        #endregion
+
+        #region Панель добавления - пол покупателя
+        private int _AddMenuGender;
+        public int AddMenuGender
+        {
+            get => _AddMenuGender;
+            set => Set(ref _AddMenuGender, value);
+        }
+        #endregion
+
+        #region Панель добавления - возраст покупателя
+        private string _AddMenuAge;
+        public string AddMenuAge
+        {
+            get => _AddMenuAge;
+            set => Set(ref _AddMenuAge, value);
+        }
+        #endregion
+
+        #region Панель добавления - имя покупателя
+        private string _AddMenuName;
+        public string AddMenuName
+        {
+            get => _AddMenuName;
+            set => Set(ref _AddMenuName, value);
+        }
+        #endregion
+
+        #region Панель добавления - список продуктов, количество
+        private Product _AddMenuSelectedProduct;
+
+        public Product AddMenuSelectedProduct
+        {
+            get => _AddMenuSelectedProduct;
+            set => Set(ref _AddMenuSelectedProduct, value);
+        }
+        #endregion
+
+        #region Команды
+
+        #region CreateCustomerCommand
+        public ICommand CreateCustomerCommand { get; }
+
+        private bool CanCreateCustomerCommandExecute(object p) => true;
+
+        private void OnCreateCustomerCommandExecuted(object p)
+        {
+
+            if (!int.TryParse(AddMenuAge, out int age))
+            {
+                age = 0;
+            }
+
+            var newProductsList = new List<Product>();
+
+            foreach(Product item in AddMenuProducts)
+            {
+                if (item.Count > 0)
+                {
+                    newProductsList.Add(new Product(item));
+                }
+            }
+
+            int sumTimeToBuy = 0;
+
+            foreach(Product element in newProductsList)
+            {
+                sumTimeToBuy += element.TimeToBuy * element.Count;
+            }
+
+            var newCustomer = new Customer
+            {
+                Name = AddMenuName ?? "Имя",
+                Gender = AddMenuGender == 0 ? "Мужчина" : "Женщина",
+                Age = age,
+                QueueEnterTime = DateTime.Now,
+                BuyTime = sumTimeToBuy,
+                Products = newProductsList
+            };
+
+            MainQueue.Add_SortQueue(newCustomer);
+            Clear_AddPanelInfo();
+            AddCustomerTimer.Start();
+            VisibleAddPanel = Visibility.Collapsed;
+        }
+        #endregion
+
+        #endregion
+
+        private void Clear_AddPanelInfo()
+        {
+            AddMenuName = "";
+            AddMenuAge = "";
+            AddMenuGender = 0;
+
+            foreach (Product item in AddMenuProducts)
+            {
+                item.Count = 0;
+            }
+
+            AddMenuSelectedProduct = null;
+
+        }
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CreateCustomerCommand = new ActionCommand(OnCreateCustomerCommandExecuted, CanCreateCustomerCommandExecute);
+
+            #endregion
+
+            Product[] AddMenuproductsList = new Product[]
+            {
+                new Product {Name = "Продкут1", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут2", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут3", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут4", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут5", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут6", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут7", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут8", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут9", Count = 0, TimeToBuy = 1},
+                new Product {Name = "Продкут10", Count = 0, TimeToBuy = 1}
+            };
+            Product[] ProductsSalesStatisicList = new Product[]
+            {
+                new Product {Name = "Продкут1", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут2", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут3", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут4", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут5", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут6", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут7", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут8", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут9", Count = 0, TimeToBuy = 0},
+                new Product {Name = "Продкут10", Count = 0, TimeToBuy = 0}
+            };
+
+            ServedCustomersList = new ObservableCollection<Customer>();
+            AddMenuProducts = new List<Product>(AddMenuproductsList);
+            MainQueue = new PriorityQueue();
+            ProductsSalesStatisic = new ObservableCollection<Product>(ProductsSalesStatisicList);
+
+            #region Таймер добавления покупателя
+            AddCustomerTimer = new DispatcherTimer(DispatcherPriority.Render)
+            {
+                Interval = TimeSpan.FromSeconds(5)
+            };
+
+            AddCustomerTimer.Tick += (sender, args) =>
+            {
+                Random rnd = new();
+                int r = rnd.Next(0, 9);
+                if (r < 6)
+                {
+                    VisibleAddPanel = Visibility.Visible;
+                    AddCustomerTimer.Stop();
+                }
+                
+            };
+
+            AddCustomerTimer.Start();
+            #endregion
+
+            #region Таймер обслуживания покупателя
+            QueueServiceTimer = new DispatcherTimer(DispatcherPriority.Render)
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            int buyTimeTemp = 0;
+            bool firstCustomerBuys = false;
+            Customer firstCustomer = null;
+            QueueServiceTimer.Tick += (sender, args) =>
+            {
+                if (MainQueue.Queue.Count > 0 && !firstCustomerBuys)
+                {
+                    firstCustomer = MainQueue.Queue.First();
+                    buyTimeTemp = firstCustomer.BuyTime;
+                    firstCustomerBuys = true;
+                }
+                else if (MainQueue.Queue.Count > 0 && firstCustomerBuys)
+                {
+                    if (buyTimeTemp != 0) buyTimeTemp--;
+                    if (buyTimeTemp == 0)
+                    {
+                        ServedCustomersList.Add(firstCustomer);
+                        MainQueue.Queue.Remove(firstCustomer);
+                        firstCustomerBuys = false;
+
+                        foreach (Product item in firstCustomer.Products)
+                        {
+                            for (int i = 0; i < ProductsSalesStatisic.Count; i++)
+                            {
+                                if (item.Name == ProductsSalesStatisic[i].Name)
+                                {
+                                    ProductsSalesStatisic[i].Count += item.Count;
+                                    ProductsSalesStatisic[i].TimeToBuy += item.TimeToBuy * item.Count;
+                                    CollectionViewSource.GetDefaultView(ProductsSalesStatisic).Refresh();
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            QueueServiceTimer.Start();
+            #endregion
+        }
+    }
+}
